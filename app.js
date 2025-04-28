bcbdc6de097211e3e37f1cd1aa3235bebbdef6e3
Bilal Khan <74666953+B4bilalkhan@users.noreply.github.com>
2025-04-28 17:49:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+function addTodo() {
+    try {
+      var todoInput = document.getElementById("todoInput");
+  
+      if (todoInput.value) {
+        var liELement = document.createElement("li");
+  
+        var ulElement = document.getElementById("items_data");
+  
+        
+  
+        var DelbtnElement = document.createElement("button");
+  
+        var DelbtnText = document.createTextNode("DELETE");
+  
+        DelbtnElement.setAttribute("onclick", "deleteSingleTodo(this)");
+  
+        DelbtnElement.setAttribute("class", "deletebtn");
+  
+        DelbtnElement.appendChild(DelbtnText);
+  
+  
+        var EditbtnElement = document.createElement("button");
+  
+        var EditbtnText = document.createTextNode("EDIT");
+  
+        EditbtnElement.appendChild(EditbtnText);
+  
+        EditbtnElement.setAttribute("onclick", "EditSingleTodo(this)");
+  
+        EditbtnElement.setAttribute("class", "Editbtn");
+  
+        var liText = document.createTextNode(todoInput.value);
+  
+        liELement.appendChild(liText);
+  
+        ulElement.appendChild(liELement);
+        liELement.appendChild(DelbtnElement);
+  
+        liELement.appendChild(EditbtnElement);
+  
+        console.log(liELement);
+  
+        todoInput.value = "";
+      } else {
+        alert("required input field");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  function deleteAllTodos() {
+    var ulElement = document.getElementById("items_data");
+    ulElement.innerHTML = "";
+  }
+  
+  function deleteSingleTodo(e) {
+    e.parentNode.remove();
+  }
+  
+  function EditSingleTodo(e) {
+    var userInput = prompt("Enter Updated value");
+  
+    e.parentNode.firstChild.nodeValue = userInput;
+  }
